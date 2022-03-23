4f001fb6db68db316346c19cd4495ad0b576c388
Adelice <nancytoniw@gmail.com>
2022-03-23 14:26:22 +0200

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(a){
+    a++;
+}
+function decrement(a){
+    a--;
+}
+function makeInt(n){
+    parseInt(10);
+}
